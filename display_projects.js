acf2a6ed89dbcad9384a0abb37f6d92ca20168cc
Sinan-Daghan <daghan.sinan@outlook.fr>
2023-05-03 05:44:07 +0000

    Refactoring the functions to display the UI

--- display_projects.js
+++ display_projects.js
@@ -3,83 +3,76 @@ let projects_list = [
     ["Vector Field", "projects/vector_field.js"]
 ]
 
-const main = document.getElementById('main');
-const nav = document.getElementById('nav');
-
-let show_home_button = () => document.getElementById('home_button').style.display = 'flex';
-let hide_home_button = () => document.getElementById('home_button').style.display = 'none';
-let show_canvas = () => { canvas.style.display = 'block'; }
-let hide_canvas = () => { canvas.style.display = 'none'; }
-
 //id of the script currently loaded
 let script_id;
 
-let create_button = () => {
-    let home = document.createElement('div');
-    home.setAttribute('id', 'home_button');
-    home.innerText = 'Home';
-    home.style.width = '200px';
-    home.style.height = '64px';
-    home.style.margin = '10px';
-    home.style.border = '1px solid black';
-    home.style.borderRadius = '0px';
-    home.style.boxShadow = '4px 4px 7px black';
-    home.style.color = 'rgba(0,0,0,0.8)';
-    home.style.position = 'absolute';
-    nav.appendChild(home);
+const main = document.getElementById('main');
+const nav = document.getElementById('nav');
+const canvas_interface = document.getElementById('canvas_interface');
 
-    home.style.display = 'none';
+let show_home_button = () => document.getElementById('home_button').style.display = 'flex';
+let hide_home_button = () => document.getElementById('home_button').style.display = 'none';
 
-    home.addEventListener('click', () => {
-        hide_canvas();
-        hide_home_button();
-        display_projects(projects_list);
+let show_canvas = () => { canvas.style.display = 'block'; }
+let hide_canvas = () => { canvas.style.display = 'none'; }
 
-        const script = document.getElementById(script_id);
-        if (script) {
-            script.remove();
-            stop_draw();
-        }
+let show_canvas_interface = () => { canvas_interface.style.display = 'flex'; }
+let hide_canvas_interface = () => { canvas_interface.style.display = 'none'; }
 
+let project_click_callback = (project) => {
+
+    main.innerHTML = '';
+    show_canvas_interface();
+    show_canvas();
+    show_home_button();
+
+    let script = document.createElement('script');
+    script.id = project[1];
+    script_id = project[1];
+    script.src = project[1];
+
+    document.body.appendChild(script);
+}
+
+let display_projects = (project_list) => {
+
+    project_list.forEach(project => {
+        // create_button = (name, id, parent, callback)
+        create_button(project[0], project[0].replace(/\s+/g, '') , main, () => { project_click_callback(project); });
+        document.getElementById(project[0].replace(/\s+/g, '')).setAttribute('class', 'project');
     })
 }
-create_button();
 
-let display_projects = (projects) => {
-
-    projects.forEach(project => {
-
-        let div = document.createElement('div');
-        div.setAttribute('id', project[0]);
-        div.style.width = '600px';
-        div.style.height = '200px';
-        div.style.margin = "10px";
-        div.style.border = '1px solid black';
-        div.style.borderRadius = '0px';
-        div.style.boxShadow = '4px 4px 7px black';
-        div.style.color = 'rgba(0,0,0,0.8)';
-
-        let paragraph = document.createElement('p');
-        let text = document.createTextNode(project[0]);
-        paragraph.appendChild(text);
-        div.appendChild(paragraph);
-
-        div.addEventListener('click', () => {
-            main.innerHTML = '';
-            show_canvas();
-            show_home_button();
-
-            let script = document.createElement('script');
-            script.setAttribute('id', project[1]);
-            script.src = project[1];
-            document.body.appendChild(script);
-            script_id = project[1];
-        })
-
-        main.appendChild(div);
-    });
+let create_button = (name, id, parent, callback) => {
+    let button = document.createElement('button');
+    button.innerText = name;
+    button.addEventListener('click', callback);
+    button.id = id;
+    parent.appendChild(button);
 }
-display_projects(projects_list);
 
-// debug
-// show_canvas();
+home_buton_click_callback = () => {
+    hide_canvas();
+    hide_home_button();
+    display_projects(projects_list);
+
+    canvas_interface.innerHTML = '';
+    hide_canvas_interface();
+
+    const script = document.getElementById(script_id);
+    if (script) {
+        script.remove();
+        stop_draw();
+    } else {
+        console.log('script not found');
+    }
+}
+
+let create_home_button = () => {
+    create_button('Home', "home_button", nav, home_buton_click_callback);
+}
+
+create_home_button();
+hide_home_button();
+hide_canvas_interface();
+display_projects(projects_list);
