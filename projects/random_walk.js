f1da9e9faaf847188f807e31b8bec9699f2b6df3
Sinan-Daghan <daghan.sinan@outlook.fr>
2023-07-29 13:36:03 -0700

    Add - buttons to change the step size of the random walk

--- projects/random_walk.js
+++ projects/random_walk.js
@@ -2,18 +2,19 @@ class Square {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.step = 1;
         this.color = 'rgba(0,0,0,1)';
     }
     move() {
         const random = Math.random();
         if (random < 0.25) {
-            this.x++;
+            this.x += this.step;
         } else if (random < 0.5) {
-            this.x--;
+            this.x -= this.step;
         } else if (random < 0.75) {
-            this.y++;
+            this.y += this.step;
         } else {
-            this.y--;
+            this.y -= this.step;
         }
     }
     draw() {
@@ -32,3 +33,19 @@ let main_loop = () => {
     }, 10);
 }
 window.requestAnimationFrame(main_loop);
+
+let button_step = create_button('step : 1', canvas_interface, null)
+
+create_button('+', canvas_interface, () => {
+    if (square.step < 30) {
+        square.step ++;
+    }
+    button_step.innerText = 'step : ' + square.step;
+});
+
+create_button('-', canvas_interface, () => {
+    if (square.step > 1) {
+        square.step --
+    }
+    button_step.innerText = 'step : ' + square.step;
+});
