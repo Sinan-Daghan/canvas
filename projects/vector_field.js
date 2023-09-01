9e530afa12eee65b5baed23c9ef22a4505fc68f8
Sinan-Daghan <daghan.sinan@outlook.fr>
2023-09-01 14:53:44 -0700

    add a button to attract or repel vectors

--- projects/vector_field.js
+++ projects/vector_field.js
@@ -1,5 +1,14 @@
-script_wrapper = () => {
-canvas_interface.style.display = 'none';
+let attract = false;
+
+let toggleAttraction = () => {
+    attract = !attract;
+    button.style.border = attract ? 'none' : '3px solid purple';
+}
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') toggleAttraction();
+});
+let button = create_button('attract \n (space)', canvas_interface, toggleAttraction);
+toggleAttraction();
 
 vector_grid = create_grid(width, height, 20);
 
@@ -10,18 +19,16 @@ document.addEventListener("mousemove", (event) => {
     Vmouse = new Vector(event.clientX - rect.left, event.clientY - rect.top);
 });
 
-// draw();
 function draw() {
     ctx.clearRect(0, 0, width, height);
     for (const Vposition of vector_grid) {
         let Vstart = Vposition;
         let Vector = Vmouse.sub(Vstart);
+
+        if (attract) Vector = Vector.mult(-1);
         draw_Vstart_Vector(Vstart, Vector.normalize(10));
     }
 
     draw_id = window.requestAnimationFrame(draw);
 
 } draw_id = window.requestAnimationFrame(draw);
-
-}
-script_wrapper();
