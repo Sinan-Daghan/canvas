fdcc6dd907570afd3f9488e663a1179f5e238128
Sinan-Daghan <daghan.sinan@outlook.fr>
2023-09-05 14:58:29 -0700

    add slider to control gravitational force

--- projects/gravity.js
+++ projects/gravity.js
@@ -2,12 +2,23 @@ let ball1 = new Ball(100, 100, 10);
 let ball2 = new Ball(150, 200, 10);
 let balls = [ball1, ball2];
 
+let G = 0.01;
+
+let gravityValue = create_button('gravityValue', canvas_interface, null);
+gravityValue.innerText = G;
+
+let slider = create_slider('gravity', canvas_interface, 0.001, 0.1, G, 0.001);
+slider.addEventListener('input', () => {
+    G = slider.value;
+    gravityValue.innerText = G;
+});
+slider.value = G;
+
 let gravityForce = (ball1, ball2) => {
     let distance = ball1.position.sub(ball2.position);
     let distance_mag = distance.get_magnitude();
     let m1 = ball1.mass;
     let m2 = ball2.mass;
-    let G = 0.01;
 
     let gravity = G * m1 * m2 / distance_mag ** 2;
     let force = distance.normalize(gravity);
