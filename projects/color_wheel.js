96472684019ef01c645f6b994285c9e93fe5ec9e
Sinan-Daghan <daghan.sinan@outlook.fr>
2023-10-07 10:57:13 -0700

    add a slider to control the wheel radius and a button to redraw

--- projects/color_wheel.js
+++ projects/color_wheel.js
@@ -6,7 +6,7 @@ let main_loop = () => {
 
     ctx.beginPath();
     ctx.strokeStyle = `hsl(${angle * 180 / Math.PI}, 100%, 50%)`;
-    ctx.moveTo(canvas.width / 2 + 100 * Math.cos(angle), canvas.height / 2 + 100 * Math.sin(angle));
+    ctx.moveTo(canvas.width / 2 , canvas.height / 2);
     ctx.lineTo(canvas.width / 2 + radius * Math.cos(angle), canvas.height / 2 + radius * Math.sin(angle));
     ctx.stroke();
     angle += increment;
@@ -14,3 +14,15 @@ let main_loop = () => {
     if (angle < 2 * Math.PI) window.requestAnimationFrame(main_loop);
 }
 window.requestAnimationFrame(main_loop);
+
+let btn_radius = create_button('Radius: 200', canvas_interface, null);
+create_slider('radius', canvas_interface, 0, 200, 200, 1).addEventListener('input', (e) => {
+    radius = e.target.value;
+    btn_radius.innerText = `Radius: ${radius}`;
+});
+
+create_button('Redraw', canvas_interface, () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    angle = 0;
+    main_loop();
+});
