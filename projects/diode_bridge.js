3ba240bbef384077aea27f507e807ce5b1e3ec57
Sinan Daghan <daghan.sinan@outlook.fr>
2023-11-21 22:53:18 +0100

    add the Bridge class to draw a diode bridge with 4 diodes

--- projects/diode_bridge.js
+++ projects/diode_bridge.js
@@ -2,8 +2,8 @@ let angle = 0;
 let angle_increment = Math.pi / 180;
 
 generator = {
-    center : new Vector(50, canvas.height / 2),
-    radius : 20,
+    center : new Vector(100, canvas.height / 2 + 40),
+    radius : 40,
     draw : function(){
         ctx.beginPath();
         ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
@@ -59,9 +59,43 @@ class Cable {
 gc = generator.center;
 gr = generator.radius;
 
-let cable_1 = new Cable(new Vector(gc.x, gc.y - gr), new Vector(gc.x + 100, gc.y - gr), -Math.PI / 2);
-let cable_2 = new Cable(new Vector(gc.x, gc.y + gr), new Vector(gc.x + 100, gc.y + gr), Math.PI / 2);
+class Bridge {
+    constructor(left, right) {
+        this.left = left;
+        this.right = right;
 
+        this.leftToMiddle = this.right.sub(this.left).div(2);
+
+        this.bottom = new Vector(this.leftToMiddle.x, this.leftToMiddle.x).add(this.left);
+        this.top = new Vector(this.leftToMiddle.x, - this.leftToMiddle.x).add(this.left);
+
+        this.firstDiode = new Diode(this.left, this.top, 'red');
+        this.secondDiode = new Diode(this.top, this.right, 'blue');
+        this.thirdDiode = new Diode(this.right, this.bottom, 'green');
+        this.fourthDiode = new Diode(this.bottom, this.left, 'yellow');
+        
+    }
+    draw() {
+        this.firstDiode.draw();
+        this.secondDiode.draw();
+        this.thirdDiode.draw();
+        this.fourthDiode.draw();
+    }
+}
+let Vmiddle = new Vector(canvas.width / 2, canvas.height / 2);
+let bridge = new Bridge(Vmiddle, new Vector(Vmiddle.x + generator.radius * 2, Vmiddle.y));
+bridge.draw();
+
+let cable_1 = new Cable(new Vector(gc.x, gc.y - gr), new Vector(gc.x + 100, gc.y - gr), -Math.PI / 2);
+let cable_2 = new Cable(new Vector(gc.x, gc.y + gr), new Vector(gc.x + (bridge.right.x - gc.x), gc.y + gr), Math.PI / 2);
+
+let calbe_3 = new Cable(bridge.top, new Vector(bridge.top.x + 200, bridge.top.y), 0);
+calbe_3.draw();
+let calbe_4 = new Cable(bridge.bottom, new Vector(bridge.bottom.x + 200, bridge.bottom.y), 0);
+calbe_4.draw();
+
+let cable_5 = new Cable(new Vector(gc.x + (bridge.right.x - gc.x), gc.y + gr), new Vector(gc.x + (bridge.right.x - gc.x), bridge.right.y), Math.PI / 2);
+cable_5.draw();
 
 main_loop = () => {
     if (angle >= 2 * Math.PI) angle = 0;
